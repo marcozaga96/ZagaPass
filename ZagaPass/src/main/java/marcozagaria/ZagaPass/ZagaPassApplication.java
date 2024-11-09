962b27b4ef13540cc6a43688c860320bb6e7a78f
Marco Zagaria <marcozagaria@live.com>
2024-11-09 19:08:58 +0100

    back_end

---
+++ ZagaPass/src/main/java/marcozagaria/ZagaPass/ZagaPassApplication.java
@@ -0,0 +1,13 @@
+package marcozagaria.ZagaPass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZagaPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZagaPassApplication.class, args);
+	}
+
+}
---
+++ ZagaPass/src/test/java/marcozagaria/ZagaPass/ZagaPassApplicationTests.java
@@ -0,0 +1,13 @@
+package marcozagaria.ZagaPass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZagaPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
